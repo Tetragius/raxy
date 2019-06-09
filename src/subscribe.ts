a5cb07f0b35a87b8630b963a02dcab23f5ffea63
tetragius <tetragius@gmail.com>
2019-06-09 12:28:56 +0300

    tests

--- src/subscribe.ts
+++ src/subscribe.ts
@@ -1,5 +1,5 @@
 export function subscribe<S, T>(store, subscribers, callback, mapper): void {
-    
+
     const hooks = {
         set: (target, name, val) => {
             if (target[name] && target[name] !== val) { subscriber.needToUpdate = true; }
@@ -8,9 +8,16 @@ export function subscribe<S, T>(store, subscribers, callback, mapper): void {
         }
     }
 
+    const proxyer = data => {
+        if (typeof data === 'object') {
+            return new Proxy(data, hooks);
+        }
+        return data;
+    }
+
     const subscriber = {
         updater: (state, cb) => { callback(state); cb(); },
-        state: new Proxy(mapper(store), hooks),
+        state: proxyer(mapper(store)),
         mapper,
         needToUpdate: false
     }
