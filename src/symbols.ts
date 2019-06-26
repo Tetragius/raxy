678877c361151161b7e2f76c90dceac16c9fac28
CHERTORYLSKY Ilya <ilya.chertorylsky@raiffeisen.ru>
2019-06-26 13:49:03 +0300

    refactor: Symbols

---
+++ src/symbols.ts
@@ -0,0 +1,9 @@
+const Symbols = {
+    parent: Symbol('parent'),
+    name: Symbol('name'),
+    id: Symbol('id'),
+    source: Symbol('source'),
+    updated: Symbol('updated'),
+}
+
+export default Symbols;
