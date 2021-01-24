38bbe125581067c67560cf8327ea2f1a7bb1daef
Tetragius <tetragius@gmail.com>
2021-01-24 14:53:49 +0300

    v 2 - fix

--- webpack-next.config.js
+++ webpack-next.config.js
@@ -32,6 +32,7 @@ module.exports = () => ({
                         plugins: [
                             ["@babel/plugin-proposal-decorators", { legacy: true }],
                             ["@babel/plugin-proposal-class-properties", { loose: true }],
+                            ["@babel/plugin-proposal-optional-chaining"]
                         ]
                     }
                 }
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ module.exports = () => ({
                         plugins: [
                             ["@babel/plugin-proposal-decorators", { legacy: true }],
                             ["@babel/plugin-proposal-class-properties", { loose: true }],
+                            ["@babel/plugin-proposal-optional-chaining"]
                         ]
                     }
                 }
