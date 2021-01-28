d83c270972edd311903f23781419fa889fefd410
Tetragius <tetragius@gmail.com>
2021-01-28 10:33:01 +0300

    fix: build

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,8 @@ module.exports = () => ({
                         plugins: [
                             ["@babel/plugin-proposal-decorators", { legacy: true }],
                             ["@babel/plugin-proposal-class-properties", { loose: true }],
-                            ["@babel/plugin-proposal-optional-chaining"]
+                            ["@babel/plugin-proposal-optional-chaining"],
+                            ["@babel/plugin-proposal-nullish-coalescing-operator"]
                         ]
                     }
                 }
