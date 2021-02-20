6df63b7888d9891063a066d734a8d9175ffeb137
Tetragius <tetragius@gmail.com>
2021-02-20 18:31:54 +0300

    test: init files

---
+++ cypress/integration/raxy.spec.ts
@@ -0,0 +1,22 @@
+import { raxy } from '../../packages/raxy/src/index';
+
+describe('Raxy', function () {
+
+    describe('Create', function () {
+
+        it('create store - object', function () {
+            raxy({ a: 1, b: 2, c: 3 });
+        });
+
+        it('create store - array', function () {
+            raxy([1, 2, 3]);
+        });
+
+        it('create store - nested', function () {
+            raxy({ a: 1, obj: { a: 1, b: 2 } });
+            raxy({ a: 1, arr: [1, 2, 3] });
+            raxy({ a: 1, obj: { a: 1, b: 2 }, arr: [1, 2, 3] });
+            raxy([{ a: 1, obj: { a: 1, b: 2 } }, { a: 1, obj: { a: 1, b: 2 }, arr: [1, 2, 3] }, { a: 1, arr: [1, 2, 3] }]);
+        });
+    });
+});
